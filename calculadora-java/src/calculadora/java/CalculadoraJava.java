012bb11e46513ae1fa543e6e41fdc074ca320328
jose-valdez12 <90970578+jose-valdez12@users.noreply.github.com>
2022-03-19 09:59:35 -0600

    calduladora java

---
+++ calculadora-java/src/calculadora/java/CalculadoraJava.java
@@ -0,0 +1,44 @@
+
+package calculadora.java;
+
+
+public class CalculadoraJava {
+    
+    //atributos 
+private int operacion1;
+private int operacion2;
+
+
+
+public CalculadoraJava(){
+
+}
+    
+public CalculadoraJava (int operacion1, int operacion2){
+        
+this.operacion1 = operacion1;
+this.operacion2 = operacion2;
+        
+}
+
+public int sumar (){
+    int suma = this.operacion1+this.operacion2;
+    return suma;
+}
+
+public int restar (){
+    int restar = this.operacion1-this.operacion2;
+    return restar;
+}
+
+public int multiplicar (){
+    int multiplicar = this.operacion1*this.operacion2;
+    return multiplicar;
+}
+
+
+public int dividir (){
+    int dividir=(double)this.operacion1/(double)this.operacion2;
+    return dividir;
+}
+}
