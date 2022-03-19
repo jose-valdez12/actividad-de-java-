259e1f8038f88398ad50520dfe55cf6336161c26
José octavio valdez miranda <90970578+jose-valdez12@users.noreply.github.com>
2022-03-19 11:17:07 -0600

    Add files via upload

--- Proyecto.java
+++ Proyecto.java
@@ -10,7 +10,21 @@ package calculadora.java;
  */
 public class Proyecto {
     public static void main (String[]args){
+        
+        CalculadoraJava calcu = new CalculadoraJava (10,2);
        
+        double suma = calcu.sumar();
+        System.out.println ("la suma es :" + suma);
+        
+         double resta = calcu.restar();
+        System.out.println ("la resta es :" + resta );
+        
+        
+        double multiplicacion = calcu.multiplicar ();
+        System.out.println ("la suma es :" + multiplicacion);
+        
+        double division = calcu.dividir();
+        System.out.println ("la suma es :" + division);
     }
     
 }
